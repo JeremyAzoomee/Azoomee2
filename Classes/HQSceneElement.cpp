22d5fa8598279c0b9a59ff64752c09ebc69c6258
Tamas Bonis <tamas.bonis@azoomee.com>
2016-11-22 15:48:32 +0000

    HQSceneElement creation in progress

---
+++ Classes/HQSceneElement.cpp
@@ -0,0 +1,72 @@
+#include "HQSceneElement.h"
+
+USING_NS_CC;
+
+Scene* HQSceneElement::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HQSceneElement::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HQSceneElement::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    //This class is responsible for displaying an element on the scrollviews. The following are set up here:
+    // - highlight (the size) of an icon
+    // - category of an icon (video, audio, game or art)
+    
+    //Structure:
+    // - we need to create a LayerColor first, cca 10px bigger than the image
+    // - we put the image on
+    // - we put the overlay on the top of the image (colour depending on the category)
+    // - we put game icon and labels on top of the overlay image
+    
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
+    fillUpColoursAndImagesArray();
+    
+    return true;
+}
+
+void HQSceneElement::addHQSceneElement(int category, int highlight, std::string filename)
+{
+    createColourLayer(category, highlight);
+    
+}
+
+void HQSceneElement::fillUpColoursAndImagesArray()
+{
+    baseColours.push_back(Color4B(255,0,0, 150));
+    baseColours.push_back(Color4B(0,255,0, 150));
+    baseColours.push_back(Color4B(0,0,255, 150));
+    baseColours.push_back(Color4B(0,255,255, 150));
+}
+
+void HQSceneElement::createColourLayer(int category, int highlight)
+{
+    Size iconSize = Size(100,100);
+    
+    if(highlight == 1) iconSize = Size(200, 100);
+    if(highlight == 2) iconSize = Size(200,200);
+    
+    baseLayer = LayerColor::create(baseColours.at(category), iconSize.width, iconSize.height);
+    baseLayer->setPosition(Point(Director::getInstance()->getVisibleOrigin().x, Director::getInstance()->getVisibleOrigin().y));
+    
+}
---
+++ Classes/HQSceneElement.h
@@ -0,0 +1,27 @@
+#ifndef __HQSCENEELEMENT_SCENE_H__
+#define __HQSCENEELEMENT_SCENE_H__
+
+#include "cocos2d.h"
+
+class HQSceneElement : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    void fillUpColoursAndImagesArray();
+    void createColourLayer(int category, int highlight);
+    
+    std::vector<cocos2d::Color4B> baseColours;
+    std::vector<std::string> iconImages;
+    
+    void addHQSceneElement(int category, int highlight, std::string filename);
+    
+    cocos2d::LayerColor *baseLayer;
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HQSceneElement);
+};
+
+#endif // __HQSCENE_SCENE_ELEMENT_H__
