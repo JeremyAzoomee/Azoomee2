7dec94903aa8112c85f56282961304ff1b29200c
Tamas Bonis <tamas.bonis@azoomee.com>
2016-11-22 09:52:52 +0000

    First, initial commit.

---
+++ proj.android/gen/com/tinizine/azoomee/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tinizine.azoomee;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ proj.android/jni/Android.mk
@@ -0,0 +1,45 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos/audio/include)
+
+LOCAL_MODULE := MyGame_shared
+
+LOCAL_MODULE_FILENAME := libMyGame
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/BaseScene.cpp \
+                   ../../Classes/ChildSelectorScene.cpp \
+                   ../../Classes/HQScene.cpp \
+                   ../../Classes/IntroScene.cpp \
+                   ../../Classes/LoginScene.cpp \
+                   ../../Classes/MainHubBgElements.cpp \
+                   ../../Classes/MainHubScene.cpp \
+                   ../../Classes/NavigationLayer.cpp \
+                   ../../Classes/WebViewScene_ios.cpp \
+                   ../../Classes/ImageContainer.cpp \
+                   ../../Classes/OomeeLayer.cpp \
+                   ../../Classes/InterSceneLoader.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+# _COCOS_HEADER_ANDROID_BEGIN
+# _COCOS_HEADER_ANDROID_END
+
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+# _COCOS_LIB_ANDROID_BEGIN
+# _COCOS_LIB_ANDROID_END
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+
+# _COCOS_LIB_IMPORT_ANDROID_BEGIN
+# _COCOS_LIB_IMPORT_ANDROID_END
