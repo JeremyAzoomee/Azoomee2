d74a814d2c4fd55204ce53ee5ec4fe51ea8b89ea
Tamas Bonis <tamas.bonis@azoomee.com>
2016-11-28 15:20:23 +0100

    Login scene creation in progress

--- Classes/LoginScene.cpp
+++ Classes/LoginScene.cpp
@@ -5,16 +5,9 @@ USING_NS_CC;
 
 Scene* LoginScene::createScene()
 {
-    // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
     auto layer = LoginScene::create();
-
-    // add layer as a child to scene
     scene->addChild(layer);
-
-    // return the scene
     return scene;
 }
 
@@ -30,12 +23,13 @@ bool LoginScene::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
+    //we add an additional layer that will slide on the screen between the username and the password.
+    //we add all input elements on this layer.
+    
+    auto loginContent = Layer::create();
+    this->addChild(loginContent);
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
@@ -48,12 +42,6 @@ bool LoginScene::init()
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
     
     auto label = Label::createWithTTF("LoginScene", "fonts/Marker Felt.ttf", 24);
     
@@ -85,11 +73,5 @@ void LoginScene::menuCloseCallback(Ref* pSender)
     #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
     
 }
