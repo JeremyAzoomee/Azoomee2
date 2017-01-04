95a045749c27c866e1a4303b6f25652b4e14e994
Tamas Bonis <tamas.bonis@azoomee.com>
2017-01-04 12:31:14 +0000

    Native Android files are added to version control.

--- proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -1,6 +1,6 @@
 /****************************************************************************
 Copyright (c) 2015 Chukong Technologies Inc.
- 
+ C
 http://www.cocos2d-x.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
