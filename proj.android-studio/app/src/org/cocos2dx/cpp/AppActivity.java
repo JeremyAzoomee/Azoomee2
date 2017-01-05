8a0d683674a59496915323a50c3d25dfe35f8c69
Clive Grace <clive.grace@azoomee.com>
2017-01-05 16:22:51 +0000

    Changed gradle to include the Crashlytics Version number as it was missed in Android. Still waiting for crash to show

--- proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -38,8 +38,8 @@ import org.xwalk.core.XWalkView;
 import java.util.HashMap;
 import java.util.Map;
 
-//import com.crashlytics.android.Crashlytics;
-//import io.fabric.sdk.android.Fabric;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class AppActivity extends Cocos2dxActivity {
 
@@ -49,7 +49,7 @@ public class AppActivity extends Cocos2dxActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = this;
-        //Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
 
     }
 
