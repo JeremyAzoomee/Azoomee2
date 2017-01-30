2855fd06eb8485f555c1ca0e7d09c32e7e52f9ef
Clive Grace <clive.grace@azoomee.com>
2017-01-30 20:37:12 +0000

    Added 409 error messages.

--- Classes/StringStorage.cpp
+++ Classes/StringStorage.cpp
@@ -94,6 +94,12 @@ std::map<std::string, std::string> StringStorage::getErrorMessageStrings(long er
             errorStrings[ERROR_BUTTON] = "OK";
             break;
             
+        case 409  :
+            errorStrings[ERROR_TITLE] = "Already Registered";
+            errorStrings[ERROR_BODY] = "The email address has already been used.";
+            errorStrings[ERROR_BUTTON] = "OK";
+            break;
+            
         case -1  :
             errorStrings[ERROR_TITLE] = "No internet";
             errorStrings[ERROR_BODY] = "No internet connection available, please try again after connecting to the internet.";
