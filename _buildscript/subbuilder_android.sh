ffd17d5e6d1ceee9411daf4c400e501e77b85cca
Tamas Bonis <tamas.bonis@azoomee.com>
2017-05-10 11:36:37 +0100

    Added new sed commands to android, no example files are required.

--- _buildscript/subbuilder_android.sh
+++ _buildscript/subbuilder_android.sh
@@ -10,21 +10,25 @@ VERSIONNUMBER=$2
 BUILDNUMBER=$3
 XWALKSTRUCTURENAME=$4
 
-sed 's/_STRUCTURENAME_/'"$STRUCTURENAME"'/g' ./exampleapplication > ./Application.mk
-sed 's/_STRUCTURENAME_/'"$STRUCTURENAME"'/g' ./examplebuildgradle > ./tempgradle.temp
-sed 's/_VERSIONNUMBER_/'"$VERSIONNUMBER"'/g' ./tempgradle.temp > ./temp2gradle.temp
-sed 's/_BUILDNUMBER_/'"$BUILDNUMBER"'/g' ./temp2gradle.temp > ./temp3gradle.temp
-sed 's/_XWALKSTRUCTURENAME_/'"$XWALKSTRUCTURENAME"'/g' ./temp3gradle.temp > build.gradle 
+rm -rf ../_replaceTestResults
+mkdir ../_replaceTestResults
 
-rm -rf *.temp
+cp ../proj.android-studio/app/jni/Application.mk ../_replaceTestResults/Application.mk
+cp ../proj.android-studio/app/build.gradle ../_replaceTestResults/build.gradle
 
-cp -r ./Application.mk ../proj.android-studio/app/jni
-rm -rf ./Application.mk
+sed -i.bak 's/APP_ABI :=.*/APP_ABI := '$STRUCTURENAME'/g' ../_replaceTestResults/Application.mk
+sed -i.bak 's/APP_ABI :=.*/APP_ABI := '$STRUCTURENAME'/g' ../_replaceTestResults/build.gradle
+sed -i.bak 's/versionName .*/versionName '\'$VERSIONNUMBER\''/g' ../_replaceTestResults/build.gradle
+sed -i.bak 's/versionCode .*/versionCode '$BUILDNUMBER'/g' ../_replaceTestResults/build.gradle
 
-cp -r ./build.gradle ../proj.android-studio/app
-rm -rf ./build.gradle
+sed -i.bak 's/org.xwalk:xwalk_core_library:\([0-9.]*\)[^'\'']*/org.xwalk:xwalk_core_library:\1-'$XWALKSTRUCTURENAME'/g'  ../_replaceTestResults/build.gradle
+sed -i.bak 's/include '\''[^'\'']'\''$/include '\'$STRUCTURENAME\''/g' ../_replaceTestResults/build.gradle
+
+cp -r ../_replaceTestResults/Application.mk ../proj.android-studio/app/jni/Application.mk
+cp -r ../_replaceTestResults/build.gradle ../proj.android-studio/app/build.gradle
 
 cocos compile -p android --android-studio -m release
 
 cp -r ../bin/release/android/app-release-signed.apk ../_builds/build-$BUILDNUMBER.apk
 rm -rf ../bin/release/android/app-release-signed.apk
+rm -rf ../_replaceTestResults
