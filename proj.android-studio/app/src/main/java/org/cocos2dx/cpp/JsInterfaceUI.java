24c4893257bebc5541157dcfd5d1dd9a754d52a3
Tamas Bonis <tamas.bonis@azoomee.com>
2017-06-16 12:35:50 +0200

    Added UI webview class and javascript interface.

---
+++ proj.android-studio/app/src/main/java/org/cocos2dx/cpp/JsInterfaceUI.java
@@ -0,0 +1,74 @@
+package org.cocos2dx.cpp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+
+
+import static com.loopj.android.http.AsyncHttpClient.log;
+
+public class JsInterfaceUI extends Activity {
+
+    private static Context mContext;
+
+    public JsInterfaceUI()
+    {
+        mContext = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = this;
+    }
+
+    @JavascriptInterface
+    public void errorOccurred()
+    {
+        log.d("error", "errorOccurred called!");
+        NativeView.errorOccurred();
+    }
+
+    @JavascriptInterface
+    public void sendMediaPlayerData(String key, String value)
+    {
+        log.d("eventhandler", "eventhandler called!");
+        NativeView.sendMediaPlayerData(key, value);
+    }
+
+    @JavascriptInterface
+    public void saveLocalDataStorage(String data)
+    {
+        JNISaveLocalDataStorage(data);
+    }
+
+    @JavascriptInterface
+    public String getLocalDataStorage()
+    {
+        return JNIGetLocalDataStorage();
+    }
+
+    @JavascriptInterface
+    public String apiRequest(String method, String responseID, String score)
+    {
+        return JNISendAPIRequest(method, responseID, score);
+    }
+
+    @JavascriptInterface
+    public String getVideoPlaylist()
+    {
+        return JNIGetVideoPlaylist();
+    }
+
+    @JavascriptInterface
+    public void exitView()
+    {
+        NativeView.exitView();
+    }
+
+    public static native void JNISaveLocalDataStorage(String data);
+    public static native String JNIGetLocalDataStorage();
+    public static native String JNISendAPIRequest(String method, String responseID, String score);
+    public static native String JNIGetVideoPlaylist();
+}
