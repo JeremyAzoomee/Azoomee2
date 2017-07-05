a99851a5a578fd3396c7ea25101e43b83e2019e3
Tamas Bonis <tamas.bonis@azoomee.com>
2017-07-05 20:15:58 +0100

    Changed child selection screen.

--- common/Classes/AzoomeeCommon/UI/ElectricDreamsTextStyles.h
+++ common/Classes/AzoomeeCommon/UI/ElectricDreamsTextStyles.h
@@ -8,12 +8,14 @@
 namespace Azoomee
 {
 
+cocos2d::Label*  createLabelWith(std::string text, std::string font, cocos2d::Color3B color, int size);
 cocos2d::Label*  createLabelHeader(std::string text);
 cocos2d::Label*  createLabelFlowMainTitle(std::string text);
 cocos2d::Label*  createLabelButtonAdultPrimary(std::string text);
 cocos2d::Label*  createLabelButtonAdultSecondary(std::string text);
 cocos2d::Label*  createLabelHeaderWhite(std::string text);
 cocos2d::Label*  createLabelBody(std::string text);
+cocos2d::Label*  createLabelChildName(std::string text);
 cocos2d::Label*  createLabelBodyCentred(std::string text);
 cocos2d::Label*  createLabelButtonChildFriendly(std::string text);
 cocos2d::Label*  createLabelRailTitle(std::string text);
