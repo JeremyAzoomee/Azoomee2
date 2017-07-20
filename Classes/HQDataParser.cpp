df3199bb20c0712a66adeab836b29bd8fab1d9ad
Tamas Bonis <tamas.bonis@azoomee.com>
2017-07-20 14:24:06 +0100

    Fixed an issue of crashing when newFlag key is not present with the contentItem.

--- Classes/HQDataParser.cpp
+++ Classes/HQDataParser.cpp
@@ -86,11 +86,15 @@ bool HQDataParser::parseHQData(std::string responseString, const char *category)
             }
             
             elementProperty["newFlag"] = "false";
-            if(!contentData["items"][key]["newFlag"].IsNull())
-                if(contentData["items"][key]["newFlag"].GetBool())
-                    elementProperty["newFlag"] = "true";
             
-            HQElements.push_back(elementProperty);
+            if(contentData["items"][key].HasMember("newFlag"))
+            {
+                if(!contentData["items"][key]["newFlag"].IsNull())
+                    if(contentData["items"][key]["newFlag"].GetBool())
+                        elementProperty["newFlag"] = "true";
+            
+                HQElements.push_back(elementProperty);
+            }
         }
     }
     
