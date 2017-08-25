cd455e78ad570ca085fd2503aa30d46a271eeb90
Clive Grace <clive.grace@azoomee.com>
2017-08-25 09:11:17 +0100

    added removed code

--- Classes/ArtsAppHQElement.cpp
+++ Classes/ArtsAppHQElement.cpp
@@ -324,10 +324,10 @@ void ArtsAppHQElement::addListenerToElement(std::string filePath, bool preview)
         
         if(rect.containsPoint(locationInNode))
         {
-            //if(preview)
-            //    AnalyticsSingleton::getInstance()->previewContentClickedEvent("","", "ARTS APP");
-            //else
-            //    AnalyticsSingleton::getInstance()->contentItemSelectedEvent("ARTS APP", "1,1");
+            if(preview)
+                AnalyticsSingleton::getInstance()->previewContentClickedEvent("","", "ARTS APP");
+            else
+                AnalyticsSingleton::getInstance()->contentItemSelectedEvent("ARTS APP", "1,1");
 
             overlayWhenTouched->setOpacity(150);
             iamtouched = true;
