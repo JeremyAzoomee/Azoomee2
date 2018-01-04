520cce38871d2a94b494c778237592329e25de22
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-01-04 13:29:22 +0000

    added properties class, applied usage to analytics singleton.  Should all be fine…

---
+++ common/Classes/AzoomeeCommon/Analytics/AnalyticsProperties.cpp
@@ -0,0 +1,95 @@
+//
+//  AnalyticsProperties.cpp
+//  AzoomeeCommon
+//
+//  Created by Macauley on 04/01/2018.
+//
+
+#include "AnalyticsProperties.h"
+
+NS_AZOOMEE_BEGIN
+
+using namespace cocos2d;
+
+static std::auto_ptr<AnalyticsProperties> sAnalyticsPropertiesSharedInstance;
+
+AnalyticsProperties* AnalyticsProperties::getInstance()
+{
+    if(!sAnalyticsPropertiesSharedInstance.get())
+    {
+        sAnalyticsPropertiesSharedInstance.reset(new AnalyticsProperties());
+    }
+    return sAnalyticsPropertiesSharedInstance.get();
+}
+
+AnalyticsProperties::~AnalyticsProperties(void)
+{
+    
+}
+
+bool AnalyticsProperties::init(void)
+{
+    return true;
+}
+
+void AnalyticsProperties::setStoredContentItemProperties(const std::map<std::string, std::string>& properties)
+{
+    _storedContentItemProperties = properties;
+}
+
+void AnalyticsProperties::addPropertyToStoredContentItemProperties(const std::string &key, const std::string &value)
+{
+    _storedContentItemProperties[key] = value;
+}
+
+std::map<std::string, std::string> AnalyticsProperties::getStoredContentItemProperties() const
+{
+    return _storedContentItemProperties;
+}
+
+void AnalyticsProperties::addGeneralProperty(const std::string& key, const std::string& value)
+{
+    _storedGeneralProperties[key] = value;
+}
+std::map<std::string, std::string> AnalyticsProperties::getStoredGeneralProperties() const
+{
+    return _storedGeneralProperties;
+}
+
+void AnalyticsProperties::setCtaWindowNodeId(const std::string& nodeId)
+{
+    _ctaWindowNodeId = nodeId;
+}
+std::string AnalyticsProperties::getCtaWindowNodeId() const
+{
+    return _ctaWindowNodeId;
+}
+
+void AnalyticsProperties::setCtaWindowGroupId(const std::string& groupId)
+{
+    _ctaWindowGroupId = groupId;
+}
+std::string AnalyticsProperties::getCtaWindowGroupId() const
+{
+    return _ctaWindowGroupId;
+}
+
+void AnalyticsProperties::setOSManufacturer(const std::string& osManufacturer)
+{
+    _OSManufacturer = osManufacturer;
+}
+std::string AnalyticsProperties::getOSManufacturer() const
+{
+    return _OSManufacturer;
+}
+
+void AnalyticsProperties::setTimeOpenedContent(time_t timeOpenedContent)
+{
+    _timeOpenedContent = timeOpenedContent;
+}
+time_t AnalyticsProperties::getTimeOpenedContent() const
+{
+    return _timeOpenedContent;
+}
+
+NS_AZOOMEE_END
