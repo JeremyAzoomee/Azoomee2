e83e31f55afae3bfe392e17617ffd7c93b7ef775
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-02-15 15:15:25 +0000

    added project to main project folder. Core Data stuff is in

---
+++ oomeemaker/Classes/DataObjects/OomeeMakerDataHandler.cpp
@@ -0,0 +1,8 @@
+//
+//  OomeeMakerDataHandler.cpp
+//  AzoomeeOomeeMaker-mobile
+//
+//  Created by Macauley on 15/02/2018.
+//
+
+#include <stdio.h>
---
+++ oomeemaker/Classes/DataObjects/OomeeMakerDataHandler.h
@@ -0,0 +1,47 @@
+//
+//  OomeeMakerDataHandler.h
+//  AzoomeeOomeeMaker
+//
+//  Created by Macauley on 15/02/2018.
+//
+
+#ifndef OomeeMakerDataHandler_h
+#define OomeeMakerDataHandler_h
+
+#include "../AzoomeeOomeeMaker.h"
+#include "OomeeMakerDataStorage.h"
+#include <AzoomeeCommon/Utils/FileDownloader.h>
+#include <AzoomeeCommon/Utils/FileZipUtil.h>
+
+NS_AZOOMEE_OM_BEGIN
+
+class OomeeMakerDataHandler : public FileDownloaderDelegate, public FileZipDelegate
+{
+private:
+    const std::string kBaseFolderName = "OomeeMaker/";
+    
+    FileDownloaderRef _fileDownloader = nullptr;
+    
+    void getPackageJson()
+    void getConfigDataZip();
+    
+    void parseOomeeData();
+    void parseCategoryData();
+    void parseOomeeItemData();
+    
+    
+public:
+    static OomeeMakerDataHandler* getinstance();
+    virtual ~OomeeMakerDataHandler();
+    void init();
+    
+    void getConfigFilesIfNeeded();
+    
+    // Delegate functions
+    void onAsyncUnzipComplete(bool success, const std::string& zipPath, const std::string& dirpath);
+    void onFileDownloadComplete(const std::string& fileString, const std::string& tag, long responseCode);
+};
+
+NS_AZOOMEE_OM_END
+
+#endif /* OomeeMakerDataHandler_h */
