963db95f59f1a4139eec5793208589a1b90a54a1
Tamas Bonis <tamas.bonis@azoomee.com>
2018-02-19 14:21:06 +0000

    iOS route works.

--- Classes/WebGameAPIDataManager.cpp
+++ Classes/WebGameAPIDataManager.cpp
@@ -29,10 +29,8 @@ bool WebGameAPIDataManager::init(void)
     return true;
 }
 
-char* WebGameAPIDataManager::handleAPIRequest(const char* method, const char* responseId, const char* score)
+char* WebGameAPIDataManager::handleAPIRequest(const char* method, const char* responseId, const char* sendData)
 {
-    cocos2d::log("request arrived: %s, %s, %s", method, responseId, score);
-    
     if(strncmp(method, "requestUsername", strlen(method)) == 0)
     {
         return createReturnStringForAPI(method, responseId, "userName", ChildDataProvider::getInstance()->getLoggedInChildName().c_str());
@@ -55,7 +53,7 @@ char* WebGameAPIDataManager::handleAPIRequest(const char* method, const char* re
     
     if(strncmp(method, "updateHighScore", strlen(method)) == 0)
     {
-        int newScore = atoi(score);
+        int newScore = atoi(sendData);
         return createReturnStringForAPI(method, responseId, "score", StringUtils::format("%d", updateCurrentHighScoreForGame(newScore)).c_str());
     }
     
@@ -67,6 +65,14 @@ char* WebGameAPIDataManager::handleAPIRequest(const char* method, const char* re
     if(strncmp(method, "requestPlaylist", strlen(method)) == 0)
     {
         return createReturnStringForAPI(method, responseId, "playlist", VideoPlaylistManager::getInstance()->getPlaylist().c_str());
+    }
+    
+    if(strncmp(method, "saveImage", strlen(method)) == 0)
+    {
+        const std::string& imageData = sendData;
+        //save image data
+        
+        return createReturnStringForAPI(method, responseId, "imageSave", "success");
     }
     
     return createReturnStringForAPI(method, responseId, "error", "error in request");
