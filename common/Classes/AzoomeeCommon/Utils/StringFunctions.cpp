d8272359a7b4a73ff94875aa63de1ff6c94cb2cf
Tamas Bonis <tamas.bonis@azoomee.com>
2018-02-21 09:04:08 +0000

    Fixing behaviour on empty token.

--- common/Classes/AzoomeeCommon/Utils/StringFunctions.cpp
+++ common/Classes/AzoomeeCommon/Utils/StringFunctions.cpp
@@ -17,7 +17,16 @@ std::vector<std::string> splitStringToVector(std::string inputString, std::strin
         pos = inputString.find(separator, prev);
         if (pos == std::string::npos) pos = inputString.length();
         std::string token = inputString.substr(prev, pos - prev);
-        if (!token.empty()) result.push_back(token);
+        
+        if (token.empty())
+        {
+            result.push_back("");
+        }
+        else
+        {
+            result.push_back(token);
+        }
+        
         prev = pos + separator.length();
     }
     while (pos < inputString.length() && prev < inputString.length());
