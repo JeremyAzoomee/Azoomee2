7ca818753097035d25cce8e54e2afdf23e38d31c
Tamas Bonis <tamas.bonis@azoomee.com>
2018-03-06 14:50:41 +0100

    Added some code style.

--- proj.android-studio/app/src/main/java/org/cocos2dx/cpp/NativeView.java
+++ proj.android-studio/app/src/main/java/org/cocos2dx/cpp/NativeView.java
@@ -35,13 +35,15 @@ public class NativeView extends XWalkActivity {
     private boolean isActivityExitRequested = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_native_view);
 
         final View decorView = getWindow().getDecorView();
 
-        if (Build.VERSION.SDK_INT >= 11) {
+        if (Build.VERSION.SDK_INT >= 11)
+        {
             int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE;
             decorView.setSystemUiVisibility(uiOptions);
         }
@@ -64,15 +66,20 @@ public class NativeView extends XWalkActivity {
 
         ImageButton closeButton = new ImageButton(this);
         if(loadingGame())
+        {
             closeButton.setImageResource(R.drawable.close_button);
+        }
         else
+        {
             closeButton.setImageResource(R.drawable.back_button);
+        }
 
         closeButton.setBackgroundColor(android.graphics.Color.TRANSPARENT);
         closeButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 if(isActivityExitRequested||!isWebViewReady)
                 {
                     return;
@@ -144,9 +151,11 @@ public class NativeView extends XWalkActivity {
         xWalkWebViewStatic = null;
 
         final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        handler.postDelayed(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 JNICalls.JNIRegisterAndroidSceneChangeEvent();
 
                 finish();
@@ -196,7 +205,8 @@ public class NativeView extends XWalkActivity {
     }
 
     @Override
-    protected void onXWalkReady() {
+    protected void onXWalkReady()
+    {
         isWebViewReady = true;
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
@@ -271,7 +281,8 @@ public class NativeView extends XWalkActivity {
     {
         super.onPause();
         JNICalls.JNIRegisterAppWentBackgroundEvent();
-        if (xWalkWebView != null && isWebViewReady) {
+        if (xWalkWebView != null && isWebViewReady)
+        {
             xWalkWebView.pauseTimers();
             xWalkWebView.onHide();
         }
@@ -282,7 +293,8 @@ public class NativeView extends XWalkActivity {
     {
         super.onResume();
         JNICalls.JNIRegisterAppCameForegroundEvent();
-        if (isWebViewReady && xWalkWebView != null) {
+        if (isWebViewReady && xWalkWebView != null)
+        {
             xWalkWebView.resumeTimers();
             xWalkWebView.onShow();
         }
@@ -290,7 +302,8 @@ public class NativeView extends XWalkActivity {
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(Configuration newConfig)
+    {
         super.onConfigurationChanged(newConfig);
 
         if(activity.getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE && isActivityExitRequested)
