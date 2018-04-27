76c9f10f3ef8e4d14d0d53ea4c1b70ea361b5955
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-04-27 16:38:15 +0100

    track orientation

--- common/Classes/AzoomeeCommon/Application.cpp
+++ common/Classes/AzoomeeCommon/Application.cpp
@@ -1,5 +1,6 @@
 #include "Application.h"
 #include "UI/Scene.h"
+#include "Analytics/AnalyticsSingleton.h"
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 
@@ -156,11 +157,13 @@ void Application::updateResolution(int newWidth, int newHeight)
     // Landscape
     if(newWidth > newHeight)
     {
+        Azoomee::AnalyticsSingleton::getInstance()->setLandscapeOrientation();
         glview->setDesignResolutionSize(designResolutionLandscapeSize.width, designResolutionLandscapeSize.height, ResolutionPolicy::NO_BORDER);
     }
     // Portrait
     else
     {
+        Azoomee::AnalyticsSingleton::getInstance()->setPortraitOrientation();
         glview->setDesignResolutionSize(designResolutionPortraitSize.width, designResolutionPortraitSize.height, ResolutionPolicy::NO_BORDER);
     }
 }
