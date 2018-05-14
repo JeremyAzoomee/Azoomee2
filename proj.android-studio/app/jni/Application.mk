6036808dd0fec682903bdce48fb7f455ed4d1e77
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-05-14 10:22:35 +0100

    reverse build script stuff

--- proj.android-studio/app/jni/Application.mk
+++ proj.android-studio/app/jni/Application.mk
@@ -3,15 +3,15 @@ APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 
-APP_ABI := arm64-v8a
+APP_ABI := armeabi-v7a
 
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
   APP_OPTIM := debug
-  APP_ABI := arm64-v8a
+  APP_ABI := armeabi-v7a
 else
   APP_CPPFLAGS += -DNDEBUG
   APP_OPTIM := release
-  APP_ABI := arm64-v8a
+  APP_ABI := armeabi-v7a
 endif
