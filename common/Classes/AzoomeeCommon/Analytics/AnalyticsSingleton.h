12d712bc92566c8b70bec524bd3ca2c0ef017869
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-05-22 16:21:27 +0100

    Separate event for non-carousel items

--- common/Classes/AzoomeeCommon/Analytics/AnalyticsSingleton.h
+++ common/Classes/AzoomeeCommon/Analytics/AnalyticsSingleton.h
@@ -92,6 +92,7 @@ public:
     void contentItemSelectedEvent(const std::string& Type);
     void contentItemSelectedEvent(const std::string& Type, const std::string& elementShape);
     void contentItemSelectedEvent(const HQContentItemObjectRef &contentItem, int rowNumber, int elementNumber, const std::string& elementShape);
+    void contentItemSelectedOutsideCarouselEvent(const HQContentItemObjectRef &contentItem);
     void contentItemProcessingStartedEvent();
     void contentItemProcessingErrorEvent();
     void contentItemIncompatibleEvent();
