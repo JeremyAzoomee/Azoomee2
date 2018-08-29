9e0332130b19c0d93caf31c23293c0217a588cb0
Macauley Scoffins <macauley.scoffins@azoomee.com>
2018-08-29 17:45:54 +0100

    More account details progress

--- Classes/EditAccountLayer.cpp
+++ Classes/EditAccountLayer.cpp
@@ -30,23 +30,31 @@ bool EditAccountLayer::init()
 
 void EditAccountLayer::onEnter()
 {
+    int lowestY = this->getContentSize().height;
+    
     _nameText = ui::Text::create("Temp", Style::Font::Medium, 107);
-    _nameText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam());
+    _nameText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,80,0,0)));
     _nameText->setTextColor(Color4B::BLACK);
     this->addChild(_nameText);
+    
+    lowestY -= (_nameText->getContentSize().height + 80);
     
     UserDefault* def = UserDefault::getInstance();
     const std::string& username = def->getStringForKey("username", "");
     def->flush();
     _emailText = ui::Text::create(username, Style::Font::Medium, 59);
-    _emailText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam());
+    _emailText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,40,0,0)));
     _emailText->setTextColor(Color4B::BLACK);
     this->addChild(_emailText);
     
+    lowestY -= (_emailText->getContentSize().height + 40);
+    
     ui::Layout* pinEditboxLayout = ui::Layout::create();
     pinEditboxLayout->setContentSize(Size(this->getContentSize().width, 190));
-    pinEditboxLayout->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam());
+    pinEditboxLayout->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,35,0,0)));
     this->addChild(pinEditboxLayout);
+    
+    lowestY -= (pinEditboxLayout->getContentSize().height + 35);
     
     Label* pinText = Label::createWithTTF("Pin", Style::Font::Medium, 59);
     pinText->setTextColor(Color4B(Style::Color::battleshipGrey));
@@ -57,6 +65,7 @@ void EditAccountLayer::onEnter()
     
     _pinEditbox = TextInputLayer::createSettingsChatTextInput(this->getContentSize().width * 0.6f);
     _pinEditbox->setCenterPosition(Vec2(pinEditboxLayout->getContentSize().width * 0.55f, pinEditboxLayout->getContentSize().height / 2));
+    _pinEditbox->setText(ParentDataProvider::getInstance()->getParentPin());
     pinEditboxLayout->addChild(_pinEditbox);
     
     ui::Button* editPinButton = ui::Button::create("res/settings/edit_text_input.png");
@@ -68,6 +77,8 @@ void EditAccountLayer::onEnter()
     passwordEditboxLayout->setContentSize(Size(this->getContentSize().width, 190));
     passwordEditboxLayout->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam());
     this->addChild(passwordEditboxLayout);
+    
+    lowestY -= passwordEditboxLayout->getContentSize().height;
     
     Label* passwordText = Label::createWithTTF("Password", Style::Font::Medium, 59);
     passwordText->setTextColor(Color4B(Style::Color::battleshipGrey));
@@ -84,6 +95,42 @@ void EditAccountLayer::onEnter()
     editPasswordButton->setPosition(Vec2(passwordEditboxLayout->getContentSize().width * 0.85f - 10, passwordEditboxLayout->getContentSize().height / 2));
     editPasswordButton->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
     passwordEditboxLayout->addChild(editPasswordButton);
+    
+    float remainingPadding = lowestY - 400;
+    
+    _accountTypeLayout = ui::Layout::create();
+    _accountTypeLayout->setBackGroundImage("res/settings/rounded_rect_half.png");
+    _accountTypeLayout->setBackGroundImageColor(Style::Color::skyBlue);
+    _accountTypeLayout->setContentSize(Size(this->getContentSize().width, 400));
+    _accountTypeLayout->setBackGroundImageScale9Enabled(true);
+    _accountTypeLayout->setLayoutParameter(CreateTopLinearLayoutParam(ui::Margin(0,remainingPadding,0,0)));
+    
+    if(ParentDataProvider::getInstance()->isPaidUser())
+    {
+        Label* subDeetsLab = Label::createWithTTF(StringUtils::format("Premium Subscription\nRenew’s on %s",ParentDataProvider::getInstance()->getBillingDate().c_str()), Style::Font::Medium, 59);
+        subDeetsLab->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+        subDeetsLab->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+        subDeetsLab->setTextColor(Color4B::WHITE);
+        subDeetsLab->setHorizontalAlignment(TextHAlignment::CENTER);
+        _accountTypeLayout->addChild(subDeetsLab);
+        
+        const std::string& billingProvider = ParentDataProvider::getInstance()->getBillingProvider();
+        if(billingProvider == "APPLE" || billingProvider == "GOOGLE" || billingProvider == "AMAZON")
+        {
+            // add button to link to sub manager
+        }
+    }
+    else
+    {
+        Label* subDeetsLab = Label::createWithTTF("Free Account\nTap Renew for a special offer", Style::Font::Medium, 59);
+        subDeetsLab->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+        subDeetsLab->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+        subDeetsLab->setTextColor(Color4B::WHITE);
+        subDeetsLab->setHorizontalAlignment(TextHAlignment::CENTER);
+        _accountTypeLayout->addChild(subDeetsLab);
+    }
+    
+    this->addChild(_accountTypeLayout);
     
     Super::onEnter();
 }
--- Classes/SettingsYourAccountPage.cpp
+++ Classes/SettingsYourAccountPage.cpp
@@ -10,6 +10,13 @@
 #include <AzoomeeCommon/UI/LayoutParams.h>
 #include <AzoomeeCommon/UI/Style.h>
 #include <AzoomeeCommon/Strings.h>
+#include <AzoomeeCommon/UI/ModalWebview.h>
+#include <AzoomeeCommon/Analytics/AnalyticsSingleton.h>
+#include <AzoomeeCommon/Data/Parent/ParentDataParser.h>
+#include <AzoomeeCommon/Audio/AudioMixer.h>
+#include <AzoomeeCommon/Data/Urls.h>
+#include "LoginLogicHandler.h"
+#include "PrivacyAndTermsLayer.h"
 
 using namespace cocos2d;
 
@@ -39,6 +46,92 @@ void SettingsYourAccountPage::onEnter()
     _editAccountLayer->setContentSize(Size(this->getContentSize().width - 100, this->getContentSize().height * 0.5f));
     _editAccountLayer->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,50,0,0)));
     this->addChild(_editAccountLayer);
+    
+    float margin = this->getContentSize().height - _headerBanner->getContentSize().height - _editAccountLayer->getContentSize().height - 200;
+    
+    _footerBanner = ui::Layout::create();
+    _footerBanner->setContentSize(Size(this->getContentSize().width, 150));
+    _footerBanner->setBackGroundColor(Style::Color::skyBlue);
+    _footerBanner->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+    _footerBanner->setLayoutParameter(CreateTopLinearLayoutParam(ui::Margin(0,margin,0,0)));
+    
+    this->addChild(_footerBanner);
+    
+    Label* logout = Label::createWithTTF("Log out", Style::Font::Medium, 75);
+    logout->setTextColor(Color4B::WHITE);
+    logout->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+    logout->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    
+    ui::Button* logoutButton = ui::Button::create();
+    logoutButton->setContentSize(logout->getContentSize());
+    logoutButton->ignoreContentAdaptWithSize(false);
+    logoutButton->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
+    logoutButton->setPosition(Vec2(_footerBanner->getContentSize().width - 25, _footerBanner->getContentSize().height * 0.5f));
+    logoutButton->addTouchEventListener([&](Ref* pSender, ui::Widget::TouchEventType eType){
+        if(eType == ui::Widget::TouchEventType::ENDED)
+        {
+            AnalyticsSingleton::getInstance()->logoutParentEvent();
+            ParentDataParser::getInstance()->logoutChild();
+            
+            AudioMixer::getInstance()->stopBackgroundMusic();
+            
+            LoginLogicHandler::getInstance()->forceNewLogin();
+        }
+    });
+    
+    logoutButton->addChild(logout);
+    
+    _footerBanner->addChild(logoutButton);
+    
+    Label* privacy = Label::createWithTTF("Privacy Policy", Style::Font::Medium, 75);
+    privacy->setTextColor(Color4B::WHITE);
+    privacy->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+    privacy->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    
+    DrawNode* privacyDrawNode = DrawNode::create();
+    privacyDrawNode->drawRect(Vec2(0, -7), Vec2(privacy->getContentSize().width, -6), Color4F::WHITE);
+    privacy->addChild(privacyDrawNode);
+    
+    ui::Button* privacyButton = ui::Button::create();
+    privacyButton->setContentSize(privacy->getContentSize());
+    privacyButton->ignoreContentAdaptWithSize(false);
+    privacyButton->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    privacyButton->setPosition(Vec2(_footerBanner->getContentSize().width * 0.5, _footerBanner->getContentSize().height * 0.5f));
+    privacyButton->addTouchEventListener([&](Ref* pSender, ui::Widget::TouchEventType eType){
+        if(eType == ui::Widget::TouchEventType::ENDED)
+        {
+            ModalWebview::createWithURL(Url::PrivacyPolicyNoLinks);
+        }
+    });
+    
+    privacyButton->addChild(privacy);
+    
+    _footerBanner->addChild(privacyButton);
+    
+    Label* terms = Label::createWithTTF("Terms of Use", Style::Font::Medium, 75);
+    terms->setTextColor(Color4B::WHITE);
+    terms->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+    terms->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    
+    DrawNode* termsDrawNode = DrawNode::create();
+    termsDrawNode->drawRect(Vec2(0, -7), Vec2(terms->getContentSize().width, -6), Color4F::WHITE);
+    terms->addChild(termsDrawNode);
+    
+    ui::Button* termsButton = ui::Button::create();
+    termsButton->setContentSize(privacy->getContentSize());
+    termsButton->ignoreContentAdaptWithSize(false);
+    termsButton->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
+    termsButton->setPosition(Vec2(25, _footerBanner->getContentSize().height * 0.5f));
+    termsButton->addTouchEventListener([&](Ref* pSender, ui::Widget::TouchEventType eType){
+        if(eType == ui::Widget::TouchEventType::ENDED)
+        {
+            ModalWebview::createWithURL(Url::TermsOfUse);
+        }
+    });
+    
+    termsButton->addChild(terms);
+    
+    _footerBanner->addChild(termsButton);
     
     Super::onEnter();
 }
