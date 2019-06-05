6813ef973d9a9718e1de680063e43283cc09fd43
Macauley Scoffins <macauley.scoffins@azoomee.com>
2019-06-05 17:14:52 +0100

    child manager class

---
+++ common/Classes/AzoomeeCommon/Data/Child/ChildManager.cpp
@@ -0,0 +1,179 @@
+//
+//  ChildManager.cpp
+//  AzoomeeCommon
+//
+//  Created by Macauley on 05/06/2019.
+//
+
+#include "ChildManager.h"
+#include "../Parent/ParentDataStorage.h"
+#include "../Parent/ParentDataProvider.h"
+#include "../../Analytics/AnalyticsSingleton.h"
+#include "../../Crashlytics/CrashlyticsConfig.h"
+
+NS_AZOOMEE_BEGIN
+
+using namespace cocos2d;
+
+static std::auto_ptr<ChildManager> sChildManagerSharedInstance;
+
+ChildManager* ChildManager::getInstance()
+{
+	if(!sChildManagerSharedInstance.get())
+	{
+		sChildManagerSharedInstance.reset(new ChildManager());
+	}
+	return sChildManagerSharedInstance.get();
+}
+
+ChildManager::~ChildManager()
+{
+	
+}
+
+std::string ChildManager::getParentOrChildId() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getId();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getId();
+	}
+}
+
+std::string ChildManager::getParentOrChildCdnSessionId() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getCDNSessionId();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getCDNSessionId();
+	}
+}
+
+std::string ChildManager::getParentOrChildApiSecret() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getAPISecret();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getAPISecret();
+	}
+}
+
+std::string ChildManager::getParentOrChildApiKey() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getAPIKey();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getAPIKey();
+	}
+}
+
+std::string ChildManager::getParentOrChildAvatarId() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getAvatar();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getAvatar();
+	}
+}
+
+std::string ChildManager::getParentOrChildName() const
+{
+	if(isChildLoggedIn())
+	{
+		return _loggedInChild->getProfileName();
+	}
+	else
+	{
+		return ParentDataStorage::getInstance()->getParent()->getDisplayName();
+	}
+}
+
+bool ChildManager::isChildLoggedIn() const
+{
+	if(!_childLoggedIn && _loggedInChild)
+	{
+		AnalyticsSingleton::getInstance()->debugEvent("childLoginFlagFalse", {{"parentId",ParentDataStorage::getInstance()->getParent()->getId()},{"childId",_loggedInChild->getId()}});
+	}
+	return _childLoggedIn && _loggedInChild;
+}
+
+ChildRef ChildManager::getLoggedInChild() const
+{
+	return _loggedInChild;
+}
+
+bool ChildManager::parseChildLoginData(const std::string &responseData)
+{
+	rapidjson::Document data;
+	data.Parse(responseData.c_str());
+	if(data.HasParseError())
+	{
+		return false;
+	}
+	
+	ChildRef child = ParentDataProvider::getInstance()->getChildForId(getStringFromJson("id", data));
+	if(!child)
+	{
+		return false;
+	}
+	
+	child->parseLoginData(data);
+	
+	_loggedInChild = child;
+	
+	UserDefault* def = UserDefault::getInstance();
+	def->setStringForKey("lastLoggedInChildId", child->getId());
+	def->flush();
+	
+	createCrashlyticsUserInfo(ParentDataProvider::getInstance()->getLoggedInParentId(), child->getId());
+	
+	_childLoggedIn = true;
+	return true;
+}
+
+void ChildManager::parseChildInventory(const std::string &inventoryData)
+{
+	rapidjson::Document data;
+	data.Parse(inventoryData.c_str());
+	if(data.HasParseError())
+	{
+		return;
+	}
+	
+	ChildRef child = _loggedInChild;
+	if(!child)
+	{
+		return;
+	}
+	
+	InventoryRef inventory = Inventory::createWithJson(data);
+	
+	child->setInventory(inventory);
+	
+}
+
+void ChildManager::loginChildOffline(const std::string &childId)
+{
+	_childLoggedIn = true;
+	ChildRef offlineChild = Child::create();
+	offlineChild->setId(childId);
+	_loggedInChild = offlineChild;
+}
+
+
+NS_AZOOMEE_END
---
+++ common/Classes/AzoomeeCommon/Data/Child/ChildManager.h
@@ -0,0 +1,49 @@
+//
+//  ChildManager.h
+//  AzoomeeCommon
+//
+//  Created by Macauley on 05/06/2019.
+//
+
+#ifndef ChildManager_h
+#define ChildManager_h
+
+#include <cocos/cocos2d.h>
+#include "../Json.h"
+#include "Child.h"
+
+NS_AZOOMEE_BEGIN
+	
+class ChildManager
+{
+		
+public:
+	static ChildManager* getInstance();
+	
+	virtual ~ChildManager();
+	
+	void setChildLoggedIn(bool loggedIn);
+	bool isChildLoggedIn() const;
+	
+	void setLoggedInChild(const ChildRef& child);
+	ChildRef getLoggedInChild() const;
+	
+	std::string getParentOrChildId() const;
+	std::string getParentOrChildCdnSessionId() const;
+	std::string getParentOrChildApiSecret() const;
+	std::string getParentOrChildApiKey() const;
+	std::string getParentOrChildAvatarId() const;
+	std::string getParentOrChildName() const;
+	
+	bool parseChildLoginData(const std::string &responseData);
+	void parseChildInventory(const std::string& inventoryData);
+	void loginChildOffline(const std::string& childId);
+	
+private:
+	bool _childLoggedIn = false;
+	ChildRef _loggedInChild = nullptr;
+};
+
+NS_AZOOMEE_END
+
+#endif /* ChildManager_h */
