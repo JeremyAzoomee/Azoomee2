ec8b1ccdc3f497383120d6c47def3748d2341846
David Mitchell <mitch@two-tails.com>
2019-06-05 15:45:01 +0100

    Setup CI Target & Build config for iOS

--- common/Classes/AzoomeeCommon/Azoomee.h
+++ common/Classes/AzoomeeCommon/Azoomee.h
@@ -1,11 +1,8 @@
 #ifndef AzoomeeCommon_Azoomee_h
 #define AzoomeeCommon_Azoomee_h
 
-
 // Some convenience macros for namespace
 #define NS_AZOOMEE_BEGIN namespace Azoomee {
 #define NS_AZOOMEE_END }
-
-//#define AZOOMEE_VODACOM_BUILD YES
 
 #endif
