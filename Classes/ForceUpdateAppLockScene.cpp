f91c73c1f94820b2f30e89824c3c9ba5bacbd40b
Macauley Scoffins <macauley.scoffins@azoomee.com>
2019-11-25 11:46:57 +0000

    updated force update scene visuals

--- Classes/ForceUpdateAppLockScene.cpp
+++ Classes/ForceUpdateAppLockScene.cpp
@@ -1,7 +1,6 @@
 #include "ForceUpdateAppLockScene.h"
 #include <AzoomeeCommon/Strings.h>
-#include <AzoomeeCommon/UI/ElectricDreamsTextStyles.h>
-#include <AzoomeeCommon/UI/ElectricDreamsDecoration.h>
+#include <AzoomeeCommon/UI/LayoutParams.h>
 #include "ForceUpdateSingleton.h"
 
 using namespace cocos2d;
@@ -15,59 +14,94 @@ bool ForceUpdateAppLockScene::init()
         return false;
     }
     
-    _visibleSize = Director::getInstance()->getVisibleSize();
+    ui::Layout* bgColour = ui::Layout::create();
+    bgColour->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+    bgColour->setBackGroundColor(Style::Color::darkIndigo);
+    bgColour->setSizeType(ui::Layout::SizeType::PERCENT);
+    bgColour->setSizePercent(Vec2(1.0f,1.0f));
+    addChild(bgColour);
     
-    addVisualComponentsToScene();
-    addUpdateButtonToScene();
+    _bgPattern = RoundedRectSprite::create();
+    _bgPattern->setTexture("res/decoration/pattern_stem_tile.png");
+    _bgPattern->setColor(Style::Color::azure);
+    _bgPattern->setRoundedCorners(false, false, false, false);
+    _bgPattern->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    _bgPattern->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+    _bgPattern->setScaleMode(RoundedRectSprite::ScaleMode::TILE);
+    addChild(_bgPattern);
+    
+    _bgGradient = LayerGradient::create();
+    _bgGradient->setStartColor(Style::Color::darkIndigo);
+    _bgGradient->setStartOpacity(155);
+    _bgGradient->setEndColor(Style::Color::darkIndigo);
+    _bgGradient->setEndOpacity(0);
+    _bgGradient->setVector(Vec2(0,1));
+    _bgGradient->setNormalizedPosition(Vec2::ANCHOR_BOTTOM_LEFT);
+    _bgGradient->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
+    addChild(_bgGradient);
+    
+    const float padding = 20.0f;
+    
+    _textLayout = ui::Layout::create();
+    _textLayout->setLayoutType(ui::Layout::Type::VERTICAL);
+    _textLayout->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    _textLayout->setNormalizedPosition(Vec2::ANCHOR_MIDDLE);
+    addChild(_textLayout);
+
+    _headerText = DynamicText::create(_("Azoomee needs to be updated!"), Style::Font::PoppinsBold(), 102);
+    _headerText->setTextHorizontalAlignment(TextHAlignment::CENTER);
+    _headerText->setTextVerticalAlignment(TextVAlignment::CENTER);
+    _headerText->setTextColor(Color4B::WHITE);
+    _headerText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam());
+    _headerText->setMaxLineWidth(1000);
+    _textLayout->addChild(_headerText);
+    
+    _bodyText = DynamicText::create(_("It looks like you are using an old version of Azoomee.\nTo continue using Azoomee, please ask a grown-up to update it to the latest version by tapping the button below."), Style::Font::PoppinsBold(), 50);
+    _bodyText->setTextHorizontalAlignment(TextHAlignment::CENTER);
+    _bodyText->setTextVerticalAlignment(TextVAlignment::CENTER);
+    _bodyText->setTextColor(Color4B::WHITE);
+    _bodyText->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,padding,0,padding)));
+    _bodyText->setMaxLineWidth(1000);
+    _textLayout->addChild(_bodyText);
+    
+    _updateButton = CTAButton::create("res/onboarding/rounded_button.png");
+    _updateButton->ignoreContentAdaptWithSize(false);
+    _updateButton->setScale9Enabled(true);
+    _updateButton->setContentSize(Size(600,140));
+    _updateButton->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    _updateButton->setLayoutParameter(CreateCenterHorizontalLinearLayoutParam(ui::Margin(0,padding,0,0)));
+    _updateButton->setColor(Style::Color::white);
+    _updateButton->setTextColour(Color4B::BLACK);
+    _updateButton->setTextFontInfo(Style::Font::PoppinsBold(), 66);
+    _updateButton->setTextAreaSizePercent(Vec2(0.9f,0.8f));
+    _updateButton->setText(_("Update"));
+    _updateButton->addTouchEventListener([this](Ref* pSender, ui::Widget::TouchEventType eType){
+        if(eType == ui::Widget::TouchEventType::ENDED)
+        {
+            Application::getInstance()->openURL(ForceUpdateSingleton::getInstance()->getUpdateUrlFromFile());
+        }
+    });
+    _textLayout->addChild(_updateButton);
+    
+    _textLayout->setContentSize(Size(0, _headerText->getContentSize().height + _bodyText->getContentSize().height + _updateButton->getContentSize().height + (3 * padding)));
 
     return true;
 }
 
-void ForceUpdateAppLockScene::addVisualComponentsToScene()
-{
-    addSideWiresToScreen(this, 0, 1);
-    
-    auto titleLabel = Label::createWithTTF(_("Azoomee needs to be updated!"), Style::Font::Regular(), 130);
-    titleLabel->setColor(Style::Color::brightAqua);
-    titleLabel->setAnchorPoint(Vec2(0.5,1.0));
-    titleLabel->setHorizontalAlignment(TextHAlignment::CENTER);
-    titleLabel->setWidth(_visibleSize.width * 0.8f);
-    titleLabel->setPosition(_visibleSize.width/2, _visibleSize.height - 100);
-    this->addChild(titleLabel);
-    
-    auto subTitleLabel = Label::createWithTTF(_("It looks like you are using an old version of Azoomee.\nTo continue using Azoomee, please ask a grown-up to update it to the latest version by tapping the button below."), Style::Font::Regular(), 76);
-    subTitleLabel->setColor(Color3B::WHITE);
-    subTitleLabel->setAnchorPoint(Vec2(0.5,1.0));
-    subTitleLabel->setHorizontalAlignment(TextHAlignment::CENTER);
-    subTitleLabel->setWidth(_visibleSize.width * 0.8f);
-    subTitleLabel->setPosition((_visibleSize.width/2),titleLabel->getPositionY() - titleLabel->getContentSize().height - 100);
-    this->addChild(subTitleLabel);
-}
-
-void ForceUpdateAppLockScene::addUpdateButtonToScene()
-{
-    updateButton = ElectricDreamsButton::createButtonWithText(_("Update Azoomee"), 225);
-    updateButton->setPosition(_visibleSize.width / 2 - updateButton->getContentSize().width / 2, updateButton->getContentSize().height * 0.75f);
-    updateButton->setDelegate(this);
-    updateButton->setMixPanelButtonName("UpdateAzoomeeButton");
-    this->addChild(updateButton);
-}
-
 void ForceUpdateAppLockScene::onSizeChanged()
 {
-    removeAllChildrenWithCleanup(true);
+    Super::onSizeChanged();
     
-    _visibleSize = Director::getInstance()->getVisibleSize();
+    const Size& contentSize = getContentSize();
     
-    addVisualComponentsToScene();
-    addUpdateButtonToScene();
-}
-
-//----------------------- Delegate Functions ----------------------------
-
-void ForceUpdateAppLockScene::buttonPressed(ElectricDreamsButton* button)
-{
-    Application::getInstance()->openURL(ForceUpdateSingleton::getInstance()->getUpdateUrlFromFile());
+    _bgPattern->setContentSize(contentSize);
+    _bgGradient->setContentSize(contentSize);
+    
+    _headerText->setMaxLineWidth(contentSize.width * 0.7f);
+    _bodyText->setMaxLineWidth(contentSize.width * 0.7f);
+    
+    const float padding = 20;
+    _textLayout->setContentSize(Size(0, _headerText->getContentSize().height + _bodyText->getContentSize().height + _updateButton->getContentSize().height + (3 * padding)));
 }
 
 void ForceUpdateAppLockScene::onExit()
